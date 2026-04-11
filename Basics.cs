c9bf6cc0523be724cd4ccd8ec5d4545a16555f0a
Fidel Salonga Colinares Jr. <123960850+Fidel-Jr@users.noreply.github.com>
2026-04-11 23:23:00 +0800

    Add user input and control flow examples

--- Basics.cs
+++ Basics.cs
@@ -58,6 +58,124 @@ namespace Basics
 
       // You should always give your variables meaningful names
       
+
+      // User Input
+      // Use Console.ReadLine() to get user input
+      Console.Write("Enter your name:");
+      string yourName = Console.ReadLine();
+      Console.WriteLine("Your name is: " + yourName);
+      
+      // User Input and Numbers
+      Console.Write("Enter your age: ");
+      int age = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Your age is: " + age);
+
+      // Concatenation and Interpolation
+      // The + operator can be used between strings to combine them. This is called concatenation
+      string firstName2 = "John ";
+      string lastName2 = "Doe";
+      string name = firstName2 + lastName2;
+      Console.WriteLine(name);
+
+      // String interpolation substitutes values of variables into placeholders in a string.
+      string firstName3 = "John";
+      string lastName3 = "Doe";
+      string name2 = $"My full name is: {firstName2} {lastName2}";
+      Console.WriteLine(name2);
+
+      
+      // If...Else
+      if (23 > 15) 
+      {
+        Console.WriteLine("23 is greater than 15"); // If 20 is greater than 18, this line will run
+      } else{
+        Console.WriteLine("23 is less than 15"); // Else this line will run instead
+      }
+
+      // Else if
+      // Use the else if statement to specify a new condition if the first condition is False
+      int age = 22;
+      if (time < 18) 
+      {
+        Console.WriteLine("You are not allowed.");
+      } 
+      else if (time < 21) 
+      {
+        Console.WriteLine("Allowed but with parents.");
+      } 
+      else 
+      {
+        Console.WriteLine("You are allowed.");
+      }
+
+      // Loops
+
+      // While Loop
+      // The while loop loops through a block of code as long as a specified condition is True
+      int i = 0;
+      while (i < 5) 
+      {
+        Console.Write(i); // 01234
+        i++;
+      }
+      
+      // For Loop
+      // When you know exactly how many times you want to loop through a block of code, use the for loop instead of while loop
+      for (int i = 0; i < 5; i++) 
+      {
+        Console.Write(i); // 01234
+      }
+
+      // Use the switch statement to select one of many code blocks to be executed.
+      int dayToday = 5;
+      switch (dayToday) 
+      {
+        case 1:
+          Console.WriteLine("Monday");
+          break; // When C# reaches a break keyword, it breaks out of the switch block.
+        case 2:
+          Console.WriteLine("Tuesday");
+          break;
+        case 3:
+          Console.WriteLine("Wednesday");
+          break;
+        case 4:
+          Console.WriteLine("Thursday");
+          break;
+        case 5:
+          Console.WriteLine("Friday");
+          break;
+        case 6:
+          Console.WriteLine("Saturday");
+          break;
+        case 7:
+          Console.WriteLine("Sunday");
+          break;
+      }
+
+      // The break statement can also be used to jump out of a loop
+      // Example in a for loop
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) // if condition is met
+        {
+          break; // this line will run and stop the loop
+        }
+        Console.WriteLine(i);
+      }
+
+      /* The continue statement breaks one iteration in the loop, if some condition is met,
+         then it continues with the next iteration in the loop */ 
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) // if condition is met
+        {
+          continue; // this line will run
+        }
+        Console.WriteLine(i); // then this skips the printing and continue to the next iteration
+      }
+      
+      
     }
   }
 }
