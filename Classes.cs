cc6edb5353853ae6fd12c51e6b593fe6f92d0f41
Fidel Salonga Colinares Jr. <123960850+Fidel-Jr@users.noreply.github.com>
2026-04-14 06:04:45 +0800

    Add Person class with constructors and demo program
    This file defines a Person class with properties for name and age, along with constructors for initialization. It also includes a Program class with a Main method that demonstrates creating instances of the Person class and displaying their details.

---
+++ Classes.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Classes
+{
+    // This class represents a person with a name and age.
+    class Person
+    {
+        public string Name { get; set; } // Class property for the name of the person.  With the get and set accessors, you can read and modify the name of the person.
+        public int Age { get; set; } // Class property for the age of the person. With the get and set accessors, you can read and modify the age of the person.
+
+        // Constructor that initializes the name and age of the person.
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Default constructor that lets you create a person without specifying a name or age.
+        public Person()
+        {
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", 21); // Creating a new instance of the Person class with the name "Jon" and age 21.
+            Person person2 = new Person("Dwayne", 30); // Creating another instance of the Person class with the name "Dwayne" and age 30.
+            // Displaying the name and age of the first person.
+            Console.WriteLine($"Person 1: Name - {person.Name} Age - {person.Age}");
+            // Displaying the name and age of the second person.
+            Console.WriteLine($"Person 1: Name - {person2.Name} Age - {person2.Age}");
+
+            Person person3 = new Person(); // Creating a new instance of the Person class using the default constructor.
+            person3.Name = "Alice"; // Setting the name of person3 to "Alice".
+            person3.Age = 25; // Setting the age of person3 to 25.
+            // Displaying the name and age of person3.
+            Console.WriteLine($"Person 3: Name - {person3.Name} Age - {person3.Age}");
+          
+        }
+    }
+}
